155e184201fe40f46900754afcdcec9124bdb178
Jonathan Goncalves da Silva <6199453@univali.br>
2023-05-25 21:20:23 -0300

    1 dia de back-end com python

---
+++ variables.py
@@ -0,0 +1,15 @@
+#dados de um paciente-usando variaveis
+nome = "Harry"
+idade=23
+status_saude="ansiedade"
+medicamento="ansiolítico"
+nome_medicamento ="alprazolam"
+historico_cardiaco_familia= False
+fumante= False
+alcool= False
+historico_outras_doencas_familia = False
+print(f'nome: {nome} , idade: {idade}')
+print(f'condição: {status_saude}')
+print(f' medicamento: {nome_medicamento}')
+print(f'é fumante? {fumante}, ingere alcool? {alcool}')
+print(f'historico familiar, cardiaca e demais doenças: {historico_cardiaco_familia}, {historico_outras_doencas_familia}')
